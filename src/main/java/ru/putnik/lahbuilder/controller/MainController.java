1492eb240bd6d7ec83ea411eeac1ae558e7ea0ac
Putnik13 <ilja.rumjancev@lenta.ru>
2019-01-06 22:19:09 +0300

    #7 Again small changes and work on ListView and object model of links of transfer function; also added button of edit links

--- src/main/java/ru/putnik/lahbuilder/controller/MainController.java
+++ src/main/java/ru/putnik/lahbuilder/controller/MainController.java
@@ -1,6 +1,10 @@
 package ru.putnik.lahbuilder.controller;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -10,9 +14,12 @@ import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.util.Callback;
+import ru.putnik.lahbuilder.Link;
 import ru.putnik.lahbuilder.axis.LogarithmicNumberAxis;
 
 import java.net.URL;
@@ -32,11 +39,13 @@ public class MainController extends Application implements Initializable {
     @FXML
     private Label tfLabel;
     @FXML
-    private ListView linksListView;
+    private ListView<Link> linksListView;
     @FXML
     private Button addLinkButton;
     @FXML
     private Button deleteLinkButton;
+    @FXML
+    private Button editLinkButton;
 
     //XYChart.Series<Integer,Integer> ser1=new XYChart.Series<>();
 
@@ -72,6 +81,55 @@ public class MainController extends Application implements Initializable {
 
         //chart.getData().add(ser1);
 
+        linksListView.setCellFactory(param -> {
 
+            ListCell<Link> listCell=new ListCell<Link>(){
+                @Override
+                protected void updateItem(Link item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if(item!=null) {
+                        setText(item.getNameLink());
+                    }
+                }
+            };
+            return listCell;
+        });
+
+        //Testing code
+        linksListView.getItems().add(new Link(Link.Type.Апериодическое1,3));
+        linksListView.getItems().add(new Link(Link.Type.Усилитель,10));
+
+        addLinkButton.setOnAction(new AddingLink());
+        deleteLinkButton.setOnAction(new DeletingLink());
+        editLinkButton.setOnAction(new EditingAddingLink());
+        linksListView.getSelectionModel().selectedIndexProperty().addListener(new ChoiceLinkOnView());
+    }
+    public class AddingLink implements EventHandler<ActionEvent>{
+
+        @Override
+        public void handle(ActionEvent event) {
+
+        }
+    }
+    public class DeletingLink implements EventHandler<ActionEvent>{
+
+        @Override
+        public void handle(ActionEvent event) {
+
+        }
+    }
+    public class EditingAddingLink implements EventHandler<ActionEvent>{
+
+        @Override
+        public void handle(ActionEvent event) {
+
+        }
+    }
+    public class ChoiceLinkOnView<Link> implements ChangeListener<Link> {
+
+        @Override
+        public void changed(ObservableValue<? extends Link> observable, Link oldValue, Link newValue) {
+
+        }
     }
 }
